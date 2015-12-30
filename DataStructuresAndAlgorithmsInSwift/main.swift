cac9a6e8a113c772949433327d2403f63bcfe50b
piggs91 <rpsr15@live.in>
2015-12-30 19:41:57 +0530

    Initial Commit

---
+++ DataStructuresAndAlgorithmsInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructuresAndAlgorithmsInSwift
+//
+//  Created by Ravi Rathore on 12/30/15.
+//  Copyright © 2015 Banago labs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
